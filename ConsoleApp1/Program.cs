195f6d172d4eac7a6c980bde162e4f703bcd3e54
Hrysia <60901627+Hrysia@users.noreply.github.com>
2020-03-06 10:21:51 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+   public  class Program
+    {
+       public static int Result(int d)
+        {
+            int k = d / 100;
+            return k;
+        }
+       public static  int Result1(int d)
+        {
+            d %= 10;
+            return d;
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a three-digit number: ");
+            int d = int.Parse(Console.ReadLine());
+            int result = Result(d);
+            int result1 = Result1(d);
+            Console.WriteLine("The first num:" + result);
+            Console.WriteLine("The last num:" + result1);
+            Console.ReadKey();
+        }
+    }
+}
